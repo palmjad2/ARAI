7198cb3927d96bbb67876cbde8f833011c717897
palmjad2 <palmjad2@isu.edu>
2025-02-06 11:48:26 -0700

    Hand Grasping

--- ARAI_Hub/Assets/Scripts/NewBehaviourScript.cs
+++ ARAI_Hub/Assets/Scripts/NewBehaviourScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Text_Manager : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
